f85b92c32c5164a7aef19c5c7294f9625b797061
Abdulkadir Aydemir <aydemir_0225@hotmail.com>
2024-01-06 21:04:06 +0300

    semantic ui install

---
+++ src/layouts/Navi.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import {
+  DropdownMenu,
+  DropdownItem,
+  MenuMenu,
+  MenuItem,
+  Button,
+  Dropdown,
+  Menu,
+  Container,
+} from "semantic-ui-react";
+
+function Navi() {
+  return (
+    <div>
+      <Menu inverted fixed>
+        <Container>
+          <MenuItem name="home" />
+          <MenuItem name="messages" />
+
+          <MenuMenu position="right">
+            <Dropdown item text="Language">
+              <DropdownMenu>
+                <DropdownItem>English</DropdownItem>
+                <DropdownItem>Russian</DropdownItem>
+                <DropdownItem>Spanish</DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+
+            <MenuItem>
+              <Button primary>Sign Up</Button>
+            </MenuItem>
+          </MenuMenu>
+        </Container>
+      </Menu>
+    </div>
+  );
+}
+
+export default Navi;
