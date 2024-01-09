d48033945d6d8b56b60c2f310a8761c835a2e965
Abdulkadir Aydemir <aydemir_0225@hotmail.com>
2024-01-09 15:25:00 +0300

    provider & redux toolkit

--- src/store/reducers/cardReducer.js
+++ src/store/reducers/cardReducer.js
@@ -1,4 +1,4 @@
-import { REMOVE_FROM_CARD } from "../actions/cardActions";
+import { ADD_TO_CARD, REMOVE_FROM_CARD } from "../actions/cardActions";
 import { cardItems } from "../initialValues/cardItems";
 
 const initialState = {
@@ -9,12 +9,15 @@ export default function cardReducer(state = initialState, { type, payload }) {
     switch (type) {
         case ADD_TO_CARD:
             let product = state.cardItems.find(c => c.product.id === payload.id)
-            break;
+            if (product) {
+                product.quantity++;
+                return { ...state }
+            } else {
+                return { ...state, cardItems: [...state.cardItems, { quantity: 1, product: payload }] }
+            }
         case REMOVE_FROM_CARD:
-
-            break;
-
+            return { ...state, cardItems: state.cardItems.filter(c => c.product.id !== payload.id) }      
         default:
-            break;
+            return state;
     }
 }
